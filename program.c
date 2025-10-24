23132e354ff5728cfd849b332278be5ae23fb29d
solene46 <solene.hasbrouck@esme.fr>
2025-10-24 08:05:11 +0000

    base de skyjo

--- program.c
+++ program.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+int main() 
+{
+ printf(" ________   _________   _________   _________"\n);
+ printf("|        | |         | |         | |         |"\n);
+ printf("|        | |         | |         | |         |"\n);
+ printf("|        | |         | |         | |         |"\n);
+ printf("|        | |         | |         | |         |"\n);
+ printf("|        | |         | |         | |         |"\n);
+ printf("|________| |_________|  _________   _________"\n);
+   return 0;
 }
+
